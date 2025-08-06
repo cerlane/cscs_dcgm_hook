6bb00d7344938df29b1fd9fc71c3909ced36aae0
Cerlane Leong <4125163+cerlane@users.noreply.github.com>
2025-08-06 09:02:34 +0200

    Update cscs_jobreport_dcgm_hook.sh
    Fix typo in if loop by removing extra colon

--- cscs_jobreport_dcgm_hook.sh
+++ cscs_jobreport_dcgm_hook.sh
@@ -31,7 +31,7 @@ common::checkcmd grep sed ldd ldconfig
 
 echo 
 
-if [ "${OCI_ANNOTATION_com__hooks__dcgm__enabled:-}" != "true" ]; then:
+if [ "${OCI_ANNOTATION_com__hooks__dcgm__enabled:-}" != "true" ]; then
     if [ "${OCI_ANNOTATION_com__hooks__com__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__com__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__hooks__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_hooks_dcgm__enabled:-}" == "true" ]; then
        printUsageError
        common::err "The name of the dcgm hook is not correctly configured in the annotation section. Please check \$SCRATCH/dcgm_hook.log for more information"
