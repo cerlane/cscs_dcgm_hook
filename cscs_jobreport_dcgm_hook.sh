18e4abe27378a10fc0c64e6f8352b6a005891336
Cerlane Leong <4125163+cerlane@users.noreply.github.com>
2025-09-17 15:28:05 +0200

    Update cscs_jobreport_dcgm_hook.sh
    This line is required. If not a pyaxis error is generated

--- cscs_jobreport_dcgm_hook.sh
+++ cscs_jobreport_dcgm_hook.sh
@@ -27,6 +27,8 @@ export PATH="${PATH}:/usr/sbin:/sbin"
 
 source "${ENROOT_LIBRARY_PATH}/common.sh"
 
+common::checkcmd grep sed ldd ldconfig
+
 if [ "${OCI_ANNOTATION_com__hooks__dcgm__enabled:-}" != "true" ]; then
     if [ "${OCI_ANNOTATION_com__hooks__com__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__com__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__hooks__hooks__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_com__dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_dcgm__enabled:-}" == "true" ] || [ "${OCI_ANNOTATION_hooks_dcgm__enabled:-}" == "true" ]; then
        printUsageError
